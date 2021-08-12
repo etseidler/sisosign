4a9d14e99d5931d9335838f0181fa80ac9272f98
Eric Seidler <eric.seidler@gmail.com>
2021-08-12 14:31:21 -0500

    convert to cli app using click; tests failing

--- sisosign/sisosign.py
+++ sisosign/sisosign.py
@@ -1,16 +1,23 @@
 import random
-import sys
 from typing import List
 
-
-def sisosign(people: List[str]) -> str:
-    return random.choice(people)
+import click
 
 
-def main() -> None:
-    p = sisosign(sys.argv[1:])
+@click.command()
+@click.argument("people", nargs=-1)
+def sisosign(people: List[str]) -> None:
+    """Can't decide who should stay on the story?
+    Why not have a computer make these tough choices for you?
+
+    PEOPLE is a list of one or more names. Only one will be chosen to stay.
+    """
+    if len(people) < 1:
+        print("Please provide at least one name or use the --help option.")
+        exit(1)
+    p = random.choice(people)
     print(f"{p} should stay")
 
 
 if __name__ == "__main__":
-    main()
+    sisosign()
